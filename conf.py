99abce3dfbccaa08c2c843327398ae270e3e1c29
discodyer <18012797086@163.com>
2020-03-17 10:09:09 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@
 """
 
 # For Maverick
-site_prefix = "/Blog-With-GitHub-Boilerplate/"
+site_prefix = "/Blog-With-GitHub-Codynet.work/"
 source_dir = "../src/"
 build_dir = "../dist/"
 index_page_size = 10
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "Codyの杂货铺"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "Cody"
+email = "cody23333@gmail.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+description = "即使你缩成一团 现实也不会有所改变哦"
+key_words = ['Maverick', 'Cody', 'blog']
 language = 'zh-CN'
 external_links = [
     {
